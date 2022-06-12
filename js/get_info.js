91225ed00753d21ffa6c49f3c51855631e3cf0bb
Nats-ji <nats_ji@hotmail.com>
2022-06-12 12:24:20 +0800

    Use prelease for experimental builds Add referece links to the upstream commit messages

--- js/get_info.js
+++ js/get_info.js
@@ -33,7 +33,7 @@ async function get_version_prev()
     return json.versions[json.versions.length - 2]
 }
 
-async function get_build(aVersion)
+async function get_latest_build_number(aVersion)
 {
     let json = await get_json(`https://papermc.io/api/v2/projects/paper/versions/${aVersion}/`)
     // in case builds are empty return -1
@@ -43,16 +43,43 @@ async function get_build(aVersion)
     return json.builds[json.builds.length - 1]
 }
 
-async function get_commit(aVersion, aBuild)
+async function get_build(aVersion, aBuild_Num)
 {
-    let json = await get_json(`https://papermc.io/api/v2/projects/paper/versions/${aVersion}/builds/${aBuild}/`)
-    return json.changes.length > 0 ? json.changes[0].commit : get_commit(aVersion, Number(aBuild) - 1);
+    let json = await get_json(`https://papermc.io/api/v2/projects/paper/versions/${aVersion}/builds/${aBuild_Num}/`)
+    return json
 }
 
-async function get_commit_msg(aVersion, aBuild)
+async function get_commit(aBuild)
 {
-    let json = await get_json(`https://papermc.io/api/v2/projects/paper/versions/${aVersion}/builds/${aBuild}/`)
-    return json.changes.length > 0 ? json.changes[0].message : "No Changes";
+    if (aBuild.changes.length > 0)
+        return aBuild.changes[0].commit
+    else
+    {
+        let pre_build = aBuild.build - 1
+        let version = aBuild.version
+        let json = await get_json(`https://papermc.io/api/v2/projects/paper/versions/${version}/builds/${pre_build}/`)
+        return get_commit(json)
+    }
+}
+
+function get_commit_msg(aBuild)
+{
+    let commit_msg = ""
+    if (aBuild.changes.length > 0)
+    {
+        function format_commit_msg(aMsg)
+        {
+            const regex = /#\d+/g;
+            return aMsg.replace(regex, "PaperMC/Paper$&");
+        }
+
+        for (const change of aBuild.changes) {
+            commit_msg += `[PaperMC/Paper@${change.commit}] ${format_commit_msg(change.summary)}\n\n`
+        }
+    }
+    else
+        commit_msg = "No Changes"
+    return commit_msg
 }
 
 async function get_released_version(aGhRepo)
@@ -69,21 +96,22 @@ async function get_released_version(aGhRepo)
       return ""
 }
 
-async function get_info(aGhRepo)
+async function get_info(aGhRepo, aVersion=null, aBuild=null)
 {
     let info = {}
-    info.latest_version = await get_version()
-    info.latest_build = await get_build(info.latest_version)
+    info.latest_version = aVersion || await get_version()
+    info.latest_build = aBuild || await get_latest_build_number(info.latest_version)
     
     // check if builds are empty, then use prev version.
     if (info.latest_build === -1)
     {
         info.latest_version = await get_version_prev()
-        info.latest_build = await get_build(info.latest_version)
+        info.latest_build = await get_latest_build_number(info.latest_version)
     }
-
-    info.latest_commit = await get_commit(info.latest_version, info.latest_build)
-    info.commit_msg = await get_commit_msg(info.latest_version, info.latest_build)
+    let build = await get_build(info.latest_version, info.latest_build)
+    info.pre_release = build.channel == "experimental"
+    info.latest_commit = await get_commit(build)
+    info.commit_msg = get_commit_msg(build)
     info.released_version = await get_released_version(aGhRepo)
     info.paper_release = convert_gh_version(info.latest_version, info.latest_build)
     return info
--- js/write_output.js
+++ js/write_output.js
@@ -6,6 +6,7 @@ function write_output(aUpdate, aInfo) {
 
   if (aUpdate && aInfo.paper_release) {
     data.Version = aInfo.paper_release;
+    data.PreRelease = aInfo.pre_release;
     data.FileName = `paper-sand-dupe-unpatched-${aInfo.latest_version}-${aInfo.latest_build}.jar`;
     data.Title = `PaperMC Sand Duplication Glitch Unpatched ${aInfo.latest_version}-${aInfo.latest_build}`;
     data.Body = `## Upstream release message\n${aInfo.commit_msg}`;
