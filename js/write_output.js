3196de91846c3fe31fe4f6099fefd1e0c4d82d85
Nats-ji <nats_ji@hotmail.com>
2022-06-13 21:49:22 +0800

    Add checksum to release body.

--- js/write_output.js
+++ js/write_output.js
@@ -1,7 +1,25 @@
 // output results
 const fs = require("fs");
+const crypto = require("crypto");
 
-function write_output(aUpdate, aInfo) {
+function file_checksum(aHashName, aPath) {
+  return new Promise((resolve, reject) => {
+    const hash = crypto.createHash(aHashName);
+    const stream = fs.createReadStream(aPath);
+    stream.on("error", (err) => reject(err));
+    stream.on("data", (chunk) => hash.update(chunk));
+    stream.on("end", () => resolve(hash.digest("hex")));
+  });
+}
+
+async function get_checksums(aPath) {
+  let md5 = await file_checksum("md5", aPath);
+  let sha1 = await file_checksum("sha1", aPath);
+  console.log(`Checksum for "${aPath}":\nMD5\t${md5}\nSHA1\t${sha1}`)
+  return `## Checksum\n\`\`\`\n${aPath}\nMD5    ${md5}\nSHA1   ${sha1}\n\`\`\``;
+}
+
+async function write_output(aUpdate, aInfo) {
   let data = { Update: aUpdate };
 
   if (aUpdate && aInfo.paper_release) {
@@ -9,7 +27,8 @@ function write_output(aUpdate, aInfo) {
     data.PreRelease = aInfo.pre_release;
     data.FileName = `paper-sand-dupe-unpatched-${aInfo.latest_version}-${aInfo.latest_build}.jar`;
     data.Title = `PaperMC Sand Duplication Glitch Unpatched ${aInfo.latest_version}-${aInfo.latest_build}`;
-    data.Body = `## Upstream release message\n${aInfo.commit_msg}`;
+    let checksum = await get_checksums(data.FileName);
+    data.Body = `## Upstream release message\n${aInfo.commit_msg}\n\n${checksum}`;
   }
 
   const json = JSON.stringify(data);
