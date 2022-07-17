ee6a5c4f74c94c39c7256914f4fe35030804fb00
Nats-ji <nats_ji@hotmail.com>
2022-07-17 20:29:28 +0800

    Add download count in release msg

--- src/write_output.js
+++ src/write_output.js
@@ -28,7 +28,8 @@ async function write_output(aUpdate, aInfo) {
     data.FileName = `paper-sand-dupe-unpatched-${aInfo.latest_version}-${aInfo.latest_build}.jar`;
     data.Title = `${aInfo.latest_version}-${aInfo.latest_build}`;
     let checksum = await get_checksums(data.FileName);
-    data.Body = `## PaperMC Sand Duplication Unpatched ${aInfo.latest_version}-${aInfo.latest_build}\n### Upstream release message\n${aInfo.commit_msg}\n${checksum}`;
+    const download_count = `[![downloads](https://img.shields.io/github/downloads/${process.env.GH_REPO}/${aInfo.paper_release}/total?label=downloads&style=flat-square)](https://github.com/${process.env.GH_REPO}/releases/download/${data.Version}/${data.FileName})`
+    data.Body = `## PaperMC Sand Duplication Unpatched ${aInfo.latest_version}-${aInfo.latest_build}\n${download_count}\n### Upstream release message\n${aInfo.commit_msg}\n${checksum}`;
   }
 
   const json = JSON.stringify(data);
